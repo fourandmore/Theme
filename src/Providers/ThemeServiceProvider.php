89f70ddebd87e488e029aa5b2cc68ef0107f2a92
fourandmore <steven.resch@four-and-more.net>
2018-11-07 20:43:09 +0100

    0.0.1

---
+++ src/Providers/ThemeServiceProvider.php
@@ -0,0 +1,32 @@
+    <?php
+     
+    namespace Theme\Providers;
+     
+    use IO\Helper\TemplateContainer;
+    use Plenty\Plugin\Events\Dispatcher;
+    use Plenty\Plugin\ServiceProvider;
+    use Plenty\Plugin\Templates\Twig;
+     
+    class ThemeServiceProvider extends ServiceProvider
+    {
+     
+    	/**
+    	 * Register the service provider.
+    	 */
+    	public function register()
+    	{
+     
+    	}
+     
+        /**
+    	 * Boot a template for the basket that will be displayed in the template plugin instead of the original basket.
+    	 */
+    	public function boot(Twig $twig, Dispatcher $eventDispatcher)
+        {
+            $eventDispatcher->listen('IO.tpl.basket', function(TemplateContainer $container, $templateData)
+            {
+                $container->setTemplate('Theme::content.ReleaseDate');
+                return false;
+            }, 0);
+        }
+    }
