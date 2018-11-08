70559363dc104bfc77609f208983f870174f7c81
fourandmore <steven.resch@four-and-more.net>
2018-11-08 14:28:30 +0100

    test2

--- src/Providers/ErscheinungsdatumServiceProvider.php
+++ src/Providers/ErscheinungsdatumServiceProvider.php
@@ -8,7 +8,7 @@
     use Plenty\Plugin\ServiceProvider;
     use Plenty\Plugin\Templates\Twig;
      
-    class ThemeServiceProvider extends ServiceProvider
+    class ErscheinungsdatumServiceProvider extends ServiceProvider
     {
       const PRIORITY = 0;
      
